8dd9a1aaf2ec19ac0297385006d6daea46b8171f
Lenovo <Lenovo@LenovoE4125>
2021-10-24 22:04:01 +0530

    initial commit

---
+++ firstweekassignment/src/firstweekassignment/JavaStringReverse.java
@@ -0,0 +1,16 @@
+package firstweekassignment;
+
+import java.io.*;
+import java.util.*;
+
+public class JavaStringReverse {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        
+        System.out.println( A.equals( new StringBuilder(A).reverse().toString()) ? "Yes" : "No" );
+        
+    }
+}
